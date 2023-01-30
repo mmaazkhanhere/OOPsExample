b088ec13fe08255b8f628ed8544e3eb42db37800
Maaz Khan <mmaazkhan@outlook.com>
2023-01-30 16:24:09 +0500

    adding colors to the console and import module

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 import { Person } from "./personClass.js";
 import { Student } from "./studentClass.js";
 
@@ -19,7 +20,7 @@ class Program{
         let myPerson= new Person();//object of Person is created
 
         myPerson.askQuestion(input.input);//askQuestion function of the Person is called here
-        console.log('You are  '+myPerson.getPersonality()); //personality of the user displayed
+        console.log(chalk.blueBright('You are  '+myPerson.getPersonality())); //personality of the user displayed
 
         let userName= await inquirer.prompt({ 
             //user enters the name 
@@ -32,12 +33,12 @@ class Program{
 
         myStudent.Name=userName.userName;//user input is passed to the proeprty Name of Student class
         //user name and personality is displayed
-        console.log(`Your name is ${myStudent.Name} and your personality type is ${myStudent.getPersonality()}`)
+        console.log(chalk.bgGray(`Your name is ${myStudent.Name} and your personality type is ${myStudent.getPersonality()}`))
         //since askQuestion for Student class is not called, default value will be shown to the user
     }
     catch{
         //the catch statement defines the code that handles the error
-        console.log('Please enter an integer value.');
+        console.log(chalk.bgRed('Please enter an integer value.'));
     }
 }
 }
